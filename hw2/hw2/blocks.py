Linear backprop fix + hp tuning

hw2/hw2/blocks.py
@@ -122,11 +122,9 @@ class Linear(Block):
         # ====== YOUR CODE: ======
         
         dx = dout@self.w
-        dw = dout.T@x
-        self.dw = self.dw + dw
-        db = dout.T@torch.ones(dout.shape[0])
-        self.db = self.db + db
-        
+        self.dw += dout.T@x
+        self.db += dout.T@torch.ones(dout.shape[0])
+
         # ========================
 
         return dx

hw2/hw2/optimizers.py
@@ -90,10 +90,13 @@ class MomentumSGD(Optimizer):
         self.reg = reg
         self.momentum = momentum
         
-        print(params[0].shape)
         # TODO: Add your own initializations as needed.
         # ====== YOUR CODE: ======
-        self.velocity = torch.zeros(params[0].shape)
+        
+        
+        
+        
+        raise NotImplementedError()      
         # ========================
 
     def step(self):
@@ -105,7 +108,7 @@ class MomentumSGD(Optimizer):
             # update the parameters tensor based on the velocity. Don't forget
             # to include the regularization term.
             # ====== YOUR CODE: ======
-            self.velocity = self.momentum*self.velocity
+            raise NotImplementedError()      
             # ========================
 
 