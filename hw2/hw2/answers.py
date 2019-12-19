671d92a3f46b1d13d17dc82b962e5a7b0263fd8c
Avishay Cohen <avishayco27@gmail.com>
2019-12-19 17:52:54 +0200

    Notebook 2 questions answered.

--- hw2/hw2/answers.py
+++ hw2/hw2/answers.py
@@ -43,26 +43,21 @@ def part2_dropout_hp():
 part2_q1 = r"""
 **Your answer:**
 
+(1) Dropout is a regularization technique with the goal of preventing overfitting.
+When the model overfits, we expect to see small train loss while the validation loss is higher.
+Comparing the dropout results against the non-dropout results shows that the training loss increases by a small margin, while the test loss decreases. This implies the model is prone to overfitting.
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+(2) The graph shows that low dropout settings has a higher test loss than what high dropout settings produces.
+Both have pretty much the same accuracy, which means that the model with the higher dropout is more stable.
+We can Infer from the above that them model is prone to overfitting (probably due to larger quantity of parameters and lack of depth).
+Even when we used dropout=0.8, a big gap between the train and test accuracy is still apparent.
+The model is apparently not suitable for the CIFAR10 dataset.
 """
 
 part2_q2 = r"""
 **Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+Yes, it is possible.
+Our model predicts a class for an image by taking the maximum over the class score and the loss.
 
 """
 # ==============
